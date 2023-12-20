182947488f7a0050bf44244313f8c696b33c4488
Hussain'z <shkhussain12@gmail.com>
2023-12-20 09:56:20 +0530

    prod db loggin false

--- packages/core/src/models/index.ts
+++ packages/core/src/models/index.ts
@@ -8,7 +8,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = require("@zap/core/src/config/config.js")[env];
 
 const  sequelize = config.url
-  ? new Sequelize(config.url, {})
+  ? new Sequelize(config.url, {logging: false})
   : new Sequelize(config.database, config.username, config.password, config);
 
 db.sequelize = sequelize;
