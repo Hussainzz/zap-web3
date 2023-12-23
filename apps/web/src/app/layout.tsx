40ab29d97b2aefa075a1f9fd6255618c25e58993
Hussain'z <shkhussain12@gmail.com>
2023-12-23 18:56:47 +0530

    detech phone

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,27 +1,33 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Saira  } from 'next/font/google'
-import { Providers } from './providers'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Saira } from "next/font/google";
+import { Providers } from "./providers";
+import { getSelectorsByUserAgent } from "react-device-detect";
+import { headers } from "next/headers";
+import NoPhone from "@/components/Common/NoPhone";
 
-const saira = Saira({ subsets: ['latin'] })
+const saira = Saira({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'ZapWeb3 | Home',
-  description: 'Empower Your Ethereum Events, Define Your Actions with ZapWeb3.',
-}
+  title: "ZapWeb3 | Home",
+  description:
+    "Empower Your Ethereum Events, Define Your Actions with ZapWeb3.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
+  const { isMobile } = getSelectorsByUserAgent(
+    headers().get("user-agent") ?? ""
+  );
+
   return (
     <html lang="en">
       <body className={saira.className} suppressHydrationWarning={true}>
-        <Providers>
-          {children}
-        </Providers>
+        {isMobile ? <NoPhone /> : <Providers>{children}</Providers>}
       </body>
     </html>
-  )
+  );
 }
---
+++ apps/web/src/components/Common/NoPhone.tsx
@@ -0,0 +1,22 @@
+const NoPhone = () => {
+  return (
+    <section className="relative py-20">
+      <div className="container">
+        <div className="h-full items-center gap-4 lg:grid lg:grid-cols-12">
+          <div className="flex h-full flex-col items-center justify-center py-10 lg:col-span-5 lg:items-start lg:py-20">
+            <h1 className="mb-6 text-center font-display text-5xl text-jacarta-700  lg:text-left lg:text-6xl">
+              No pocket-sized screens here: our app is all about big browsers!
+            </h1>
+          </div>
+        </div>
+        <div className="col-span-7">
+            <div className="relative text-center text-7xl">
+              🥲
+            </div>
+          </div>
+      </div>
+  </section>
+  );
+};
+
+export default NoPhone;
